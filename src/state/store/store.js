c19d5e52510e226e4a5706c3770de802d9b03ab0
Egor <egornovaev@mail.ru>
2019-12-10 18:13:27 +0300

    add redux devTools

--- src/state/store/store.js
+++ src/state/store/store.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from '../reducers/rootReducer';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
